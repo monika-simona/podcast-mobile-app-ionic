edba58b48c7908bf672d5bcf56f0cd74a5a47031
monika-simona <tancic.simona00@gmail.com>
2025-09-15 19:30:38 +0200

    dodavanje ruta u app i kriranje veye sa beckendom u api

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,61 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import { AuthProvider } from "./context/AuthContext";
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+/* Pages */
+import Home from "./pages/Home";
+import PodcastDetails from "./pages/PodcastDetails";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+/* Components */
+import NavBar from "./components/NavBar";
+import { AudioPlayerProvider } from "./context/AudioPlayerContext";
+import PlayerShell from "./components/PlayerShell";
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
+/* Ionic CSS */
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+import "@ionic/react/css/palettes/dark.system.css";
 
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
-import './theme/variables.css';
-import PodcastDetails from './pages/PodcastDetails';
+/* Theme */
+import "./theme/variables.css";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        <Route exact path="podcast"> 
-          <PodcastDetails />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <AuthProvider>
+        <AudioPlayerProvider>
+          <NavBar />
+          <PlayerShell>
+            <IonReactRouter>
+              <IonRouterOutlet id="main-content">
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/podcasts/:id" component={PodcastDetails} />
+                {/* Default route */}
+                <Route exact path="/" render={() => <Redirect to="/home" />} />
+              </IonRouterOutlet>
+            </IonReactRouter>
+          </PlayerShell>
+        </AudioPlayerProvider>
+      </AuthProvider>
+    </IonApp>
+  );
+};
 
 export default App;
--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // ovde ćemo kasnije zameniti IP da bi radilo i na telefonu
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://127.0.0.1:8000/api", // promeni na tvoj backend URL
+  withCredentials: true, // za Sanctum autentifikaciju
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token"); // čuvamo token u localStorage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
