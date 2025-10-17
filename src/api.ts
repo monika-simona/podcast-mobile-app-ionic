2dbe904b410c16d1120378de0ad92b9835821373
monika-simona <tancic.simona00@gmail.com>
2025-10-17 07:27:39 +0200

    nzm adroid snstalacija

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+ baseURL: "http://127.0.0.1:8000/api",
+//baseURL: "http://192.168.1.8:8000/api",
   withCredentials: true, // za Sanctum autentifikaciju
 });
 
